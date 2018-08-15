b122ebce3812f7542c0bbae0c1ccc9b3d56269d7
Xuechaohui <1126686819@qq.com>
2018-08-15 11:06:39 +0800

    第二次更新：生成数据库

--- HousingManagementSystem/settings.py
+++ HousingManagementSystem/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'HousingManagementSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crm',
+        'USER': 'root',
+        'PASSWORD': 'xue123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
